6d795483c5dbe3e2dd978bca43c52f9d38da7c24
Hyhang <hyhang1996@gmail.com>
2019-06-14 21:30:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import {BrowserRouter,Route,Switch, Redirect} from 'react-router-dom'
 import Login from './pages/login/login'
 import Admin  from './pages/admin/admin'
 
@@ -9,6 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path='/login' component={Login}/>
+          <Redirect to='/login'/>
           <Route path='/' component={Admin}/>
         </Switch>
       </BrowserRouter>
